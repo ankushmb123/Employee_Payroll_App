d60c35543a5d7ba598cb8f62f2aba43072e7b039
Ankush Bahekar <ankushmb9@gmail.com>
2022-05-09 23:08:31 +0530

    [Ankush] Add: Perform Validation Employee Payroll Data Name

--- Js/EmployeePayrollValidation.js
+++ Js/EmployeePayrollValidation.js
@@ -1,20 +1,75 @@
-//UC9-validate name
-// textContent property sets or returns the text content of the specified node, and all its descendants.
-const text = document.querySelector('#name');
+class EmployeePayroll {
 
-const textError = document.querySelector('.text-error');
-//this will throw an event showing that name is incorrect or nothing
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
 
-text.addEventListener('input', function () 
-{
-    //regex for Name
-    let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,}$');
-    if (nameRegex.test(text.value)) 
-    {
-        textError.textContent = "";
+    get name() {
+        return this._name;
     }
-    else 
-    {
-        textError.textContent = "Name is incorrect";
+    set name(name) {
+        let nameRegex = /[A-Z][a-z]{2,}/;
+        if (nameRegex.test(name))
+            this._name = name;
+        else
+            throw "Invalid Name";
     }
-});
+
+    get picture() {
+        return this._picture;
+    }
+    set picture(picture) {
+        this._picture = picture;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        if (startDate <= new Date()) {
+            this._startDate = startDate + 1;
+        } else {
+            throw "Invalid date";
+        }
+    }
+
+    get notes() {
+        return this._notes;
+    }
+
+    set notes(notes) {
+        this._notes = notes;
+    }
+
+    toString() {
+        return "id = " + this.id + ", name = " + this.name + ", gender = " + this.gender + ", profile picture = " + this.picture + ", department = " + this.department + ", salary = " + this.salary + ", startDate = " + this.startDate + ", note = " + this.notes;
+    }
+}
